2db2cbe52bf6aef610a0b5d96f250044ff9ed9c5
fatima tuana <fatima__tuana@hotmail.com>
2024-03-17 22:40:54 +0100

    created landing page

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div>
+            <v-img src="/img/heads-up-bg-logo.png"/>
+        </div>
+        <div class="centered-content  glow">
+                    <button class="start-button glow" @click="goToOverviewPage()">START</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            goToOverviewPage() {
+                this.$router.push("/");
+            }
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.centered-content{
+    display: flex;
+    align-content: center;
+    justify-content: center;
+}
+.start-button{
+    // background-color: #4e5b66;
+    // background-color: red;
+    // background-color: #ffefc2;
+    background-color: #E6B83C;
+    position: absolute;
+    bottom: 5vh;
+    font-size: 28px;
+    padding: 8px 48px;
+    border-radius: 20px;
+    // color: #E6B83C;
+    color: #ffffff;
+    // color: #4e5b66;
+    // color: #f2e2b6;
+    border: 2px solid #f2e2b6;
+
+    text-shadow: 0 0 3px #E6B83C, 0 0 5px #4e5b66;
+
+}
+
+.glow{
+    -webkit-box-shadow:0px 0px 16px 10px rgba(230,165,60,0.9);
+-moz-box-shadow: 0px 0px 16px 10px rgba(230,165,60,0.9);
+box-shadow: 0px 0px 16px 5px rgba(230,165,60,0.9);
+}
+
+</style>
